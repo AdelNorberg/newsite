35a3bcc482f1e96808bbeee8776c1e09d38feb99
Adel <darzemanov101@mail.ru>
2019-02-01 00:03:03 +0300

    new static res + обновленная гл. страница

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,13 +3,16 @@
     <div class="container">
       <header class="header">
         <div class="header-kweebec"></div>
-        <div>Kweebec</div>
+        <div class="header-text">Kweebec</div>
       </header>
       <nav class="nav-bar">
         <div>
+          <nuxt-link to="/">
+            <home class="home-icon nav-bar-list" />
+          </nuxt-link>
           <nuxt-link v-for="(item, key) in routers" :to="'/' + item.router" :key="key" class="nav-bar-list">{{ item.name }}</nuxt-link>
         </div>
-        <div>Войти</div>
+        <div class="sign-button"><nuxt-link to="sign" class="sign">Войти</nuxt-link></div>
       </nav>
       <nuxt />
       <footer class="footer">Footer</footer>
@@ -18,11 +21,15 @@
 </template>
 
 <script>
+import home from "../assets/home.vue"
+
 export default {
+  components: {
+    home
+  },
   data() {
     return {
       routers: [
-        { name: 'Главная', router: '' },
         { name: 'Моды', router: 'mods' },
         { name: 'Карты', router: 'maps' },
         { name: 'Сборки', router: 'assembly' },
@@ -33,8 +40,7 @@ export default {
 }
 </script>
 
-
-<style>
+<style lang="scss">
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
@@ -60,7 +66,7 @@ a {
   text-decoration: none;
 }
 
-a:hover, h1, h2, h3, h4, .footer, .header, .nav-bar {
+h1, h2, h3, h4, .footer, .header, .nav-bar {
   color: white;
 }
 
@@ -68,10 +74,14 @@ a:hover, h1, h2, h3, h4, .footer, .header, .nav-bar {
 .default-layout {
   display: flex;
   justify-content: center;
+  min-width: 1440px;
+  background: url("./../static/Hytale/bg.webp") no-repeat, url("./../static/Hytale/footer.webp") no-repeat;
+  background-size: 115%;
 }
 
 .container {
   width: 1440px;
+  padding: 0 20px 0 20px;
 }
 
 .header {
@@ -80,31 +90,85 @@ a:hover, h1, h2, h3, h4, .footer, .header, .nav-bar {
   justify-content: center;
   align-items: center;
   font-size: 50px;
+  margin-top: 4rem;
+  margin-bottom: 1rem;
+  font-weight: 700;
 }
 
 .header-kweebec {
-  height: 5rem;
+  height: 8rem;
+  width: 10rem;
   background: url("./../static/images/kweebec.png") no-repeat;
   background-size: 100%;
 }
 
-.footer {
-  margin-top: 2rem;
-  margin-bottom: 1rem;
+.header-text {
+  margin-right: 6rem;
 }
 
 .nav-bar {
   display: flex;
-  flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  height: 50px;
-  background-color: #91B6C6;
+  height: 3.5rem;
+  background-color: #1b304f;
   font-size: 25px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.5), inset 0 0 0 2px #4e5765;
+  border-radius: 5px;
+  position: relative;
+  border: 2px solid #15243a;
+  // :before {
+  //   top: 0;
+  //   left: 0;
+  //   position: absolute;
+  //   content: "";
+  //   display: block;
+  //   width: 14px;
+  //   height: 14px;
+  //   background: url("./../static/images/container-corner.png") no-repeat 0 0;
+  //   background-size: 14px;
+  //   transform: rotate(180deg);
+  // }
+  // :after {
+  //   top: 0;
+  //   right: 0;
+  //   position: absolute;
+  //   content: "";
+  //   display: block;
+  //   width: 14px;
+  //   height: 14px;
+  //   background: url("./../static/images/container-corner.png") no-repeat 0 0;
+  //   background-size: 14px;
+  //   transform: rotate(-90deg);
+  // }
 }
 
 .nav-bar-list {
-  padding: 0 0 0 0.8rem;
+  padding-left: 0.9rem;
+  color: #a5b9c6;
+  :hover {
+    color: #edb548;
+  } 
+}
+
+.home-icon {
+  height: 1.5rem;
+  padding-top: 0.2rem;
+}
+
+.sign-button {
+  display: inline-block;
+  position: relative;
+  font-weight: 600;
+  z-index: 0;
+  height: 1.7em;
+  padding: 0.1rem 1.2em 0 1.2em;
+  margin-right: 0.2rem;
+  border-radius: 5px;
+  border: 1px solid #a17f3c;
+  background: linear-gradient(to bottom, #3583b4, #225b8b);
+  box-sizing: border-box;
+  box-shadow: inset 0 0 0 2px rgba(0, 0, 0, 0.3);
 }
 
 .footer {
@@ -113,6 +177,9 @@ a:hover, h1, h2, h3, h4, .footer, .header, .nav-bar {
   justify-content: center;
   align-items: center;
   font-size: 20px;
-  background-color: #91B6C6;
+  margin-top: 2rem;
+  margin-bottom: 1rem;
+  background-color: #10161E;
+  box-shadow: inset 0 0 0 1px #2c2b22;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,30 @@
 <template>
   <div>
-    <div class="carousel-container">
+    <div class="preview-popular">
       <h1>Популярное</h1>
-      <div class="preview-popular">
-        <h2>Вариативные отношения Hytale</h2>
-      </div>
-      <div class="preview-popular-mini">
-        <div class="preview-mini">
-          <h3>Серверные настройки</h3>
+      <div class="preview-container">
+        <div class="preview-images">
+          <h2>Вариативные отношения Hytale</h2>
         </div>
         <div class="preview-mini">
-          <h3>Графика в Hytale</h3>
+          <div class="preview-images-mini">
+            <h3>Серверные настройки</h3>
+          </div>
+          <div class="preview-images-mini">
+            <h3>Графика в Hytale</h3>
+          </div>
+        </div>
+        <div class="bar">
         </div>
       </div>
     </div>
     
     <div class="carousel-container">
       <div class="carousel-text-header">
-        <h2>Моды</h2>
+        <h2 class="header-text">Моды</h2>
         <nuxt-link to="/mods">Показать всё...</nuxt-link>
       </div>
-      <hooper :items-to-show="3.5" :items-to-slide="1" :wheelControl="false" class="carousel">
+      <hooper :items-to-show="3.8" :items-to-slide="1" :wheelControl="false" class="carousel">
         <slide v-for="(slide, key) in carousels[0]" :key="key">
           {{ slide.name }}
         </slide>
@@ -30,10 +34,10 @@
 
     <div class="carousel-container">
       <div class="carousel-text-header">
-        <h2>Карты</h2>
+        <h2 class="header-text">Карты</h2>
         <nuxt-link to="/maps">Показать всё...</nuxt-link>
       </div>
-      <hooper :items-to-show="3.5" :items-to-slide="1" :wheelControl="false" class="carousel">
+      <hooper :items-to-show="3.8" :items-to-slide="1" :wheelControl="false" class="carousel">
         <slide v-for="(slide, key) in carousels[1]" :key="key">
           {{ slide.name }}
         </slide>
@@ -74,7 +78,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .carousel-container {
   margin-top: 5rem;
 }
@@ -91,10 +95,52 @@ export default {
 }
 
 .preview-popular {
-  height: 30rem;
+  margin-top: 2rem;
+}
+
+.preview-container {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 1rem;
+}
+
+.preview-images {
+  height: 25rem;
   width: 45rem;
-  background: url("./../static/images/media/screenshots/1-thumb.jpg") no-repeat;
+  background: url("./../static/hytale/screenshots/2.jpg") no-repeat;
   background-size: 100%;
+}
+
+.preview-mini {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.preview-images-mini {
+  height: 11rem;
+  width: 20rem;
+  background: url("./../static/hytale/screenshots/8.jpg") no-repeat;
+  background-size: 100%;
+}
+
+.preview-images-mini:nth-child(2) {
+  height: 11rem;
+  width: 20rem;
+  background: url("./../static/hytale/screenshots/5.jpg") no-repeat;
+  background-size: 100%;
+}
+
+.bar {
+  height: 25rem;
+  width: 18rem;
+  background-color: #518aa3;
+}
+
+.header-text {
+  background: -webkit-linear-gradient(#ffe98d, #e19f27);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
 }
 
 .hooper-slide {
@@ -107,6 +153,26 @@ export default {
   align-items: center;
   color: #ffffff;
   font-size: 30px;
+}
+
+.hooper-slide:nth-child(1) {
+  background: url("./../static/hytale/screenshots/9.jpg") no-repeat;
+  background-size: 100%;
+}
+
+.hooper-slide:nth-child(2) {
+  background: url("./../static/hytale/screenshots/8.jpg") no-repeat;
+  background-size: 100%;
+}
+
+.hooper-slide:nth-child(3) {
+  background: url("./../static/hytale/screenshots/9.jpg") no-repeat;
+  background-size: 100%;
+}
+
+.hooper-slide:nth-child(4) {
+  background: url("./../static/hytale/screenshots/10.jpg") no-repeat;
+  background-size: 100%;
 }
 
 .hooper-slide:first-child {
